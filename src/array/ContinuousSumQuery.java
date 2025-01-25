f636e68be7710433cdf860e75b40a157ed3c4433
Naveen Kumar <naveencoke3@gmail.com>
2025-01-25 12:46:43 +0530

    package changed

--- src/array/ContinuousSumQuery.java
+++ src/array/ContinuousSumQuery.java
@@ -1,4 +1,4 @@
-import java.awt.desktop.SystemEventListener;
+package array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
--- src/array/MaxSumOfSubArray.java
+++ src/array/MaxSumOfSubArray.java
@@ -1,3 +1,5 @@
+package array;
+
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
